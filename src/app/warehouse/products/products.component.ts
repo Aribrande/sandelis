1e089900e128cd063f542bc411fcdaa51d04870d
GediminasGricius <g.gricius@ituostas.lt>
2021-09-28 19:34:26 +0300

    20210928

--- src/app/warehouse/products/products.component.ts
+++ src/app/warehouse/products/products.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-
+  pridetiVisible=false;
   @Input() products;
   @Output() orderEmitter=new EventEmitter<{name:string}>();
 
@@ -15,9 +15,28 @@ export class ProductsComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  order(name:string){
-    console.log("Uzsakomas: "+name);
+  order(event, name:string){
+    console.log(event.target.id);
     this.orderEmitter.emit({name:name});
+  }
+
+  addProduct( inputPavadinimas:HTMLInputElement, 
+              inputKaina:HTMLInputElement, 
+              inputKiekis:HTMLInputElement, 
+              inputRezervuota:HTMLInputElement ){
+                console.log(inputPavadinimas);
+    console.log("Pridedame: "+inputPavadinimas.value+" "+inputKaina.value+" "+inputKiekis.value+" "+inputRezervuota.value);
+    
+    this.products.push({
+        name:     inputPavadinimas.value,
+        price:    parseFloat(inputKaina.value),
+        count:    parseInt( inputKiekis.value),
+        reserved: parseInt(inputRezervuota.value) 
+    });
+
+    console.log(this.products);
+
+    inputPavadinimas.value="";
   }
 
 }
